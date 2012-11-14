a2b2fe88ec4acf0a79c2eb32046d776a15acfbe3
F. Schweiger <schreppchen@gmail.com>
2012-11-14 19:30:23 +0100

    INITIAL

---
+++ src/de/frizzware/pacrun/GameActivity.java
@@ -0,0 +1,14 @@
+package de.frizzware.pacrun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	
+}
