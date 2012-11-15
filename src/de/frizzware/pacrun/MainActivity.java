d8100785471f19b9dc9f4f92e9bead5dfcb29aae
F. Schweiger <schreppchen@gmail.com>
2012-11-15 04:25:34 +0100

    ANIM

--- src/de/frizzware/pacrun/MainActivity.java
+++ src/de/frizzware/pacrun/MainActivity.java
@@ -18,7 +18,13 @@ public class MainActivity extends Activity {
 	
 	//Animationen
 	private Animation leftRightIn;
+	private Animation fadeIN;
+	private Animation fadeOUT;
+	private Animation leftRightOut;
+	
 	private ImageView pacMan;
+	private ImageView dot1;
+	private ImageView dot2;
 	
 	//MediaPlayer
 	MediaPlayer welcomeSound;
@@ -37,19 +43,32 @@ public class MainActivity extends Activity {
     private void initGUI(){
     	//Animationen lesen
     	leftRightIn = AnimationUtils.loadAnimation(this, R.anim.push_right_in);
+    	leftRightOut = AnimationUtils.loadAnimation(this, R.anim.push_right_out);
+    	fadeIN = AnimationUtils.loadAnimation(this, R.anim.fadein);
+    	fadeOUT = AnimationUtils.loadAnimation(this, R.anim.fadeout);
     	
     	//MediaPlayer erstellen
     	welcomeSound = MediaPlayer.create(this, R.raw.opening_sounds);
     	
+    	dot1 = (ImageView) findViewById(R.id.dot1);
+    	dot2 = (ImageView) findViewById(R.id.dot2);
+    	
+    	dot1.startAnimation(fadeIN);
+    	dot2.startAnimation(fadeIN);
+    	
     	ivTitlePac = (ImageView) findViewById(R.id.ivTitlePac);
     	ivTitlePac.startAnimation(leftRightIn);
+    	
     	
     	pacMan = (ImageView) findViewById(R.id.ivTitlePac);
     	pacMan.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent(MainActivity.this, GameActivity.class));
+				ivTitlePac.startAnimation(leftRightOut);
+				dot1.startAnimation(fadeOUT);
+				dot2.startAnimation(fadeOUT);
+				//startActivity(new Intent(MainActivity.this, GameActivity.class));
 			}
 		});
     }
